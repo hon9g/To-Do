568fcae9b918343033afd1228996739f25102d5c
hon9g <mimmh93@gmail.com>
2022-09-08 16:42:25 +0900

    [doc] TO-DO 앱의 기본 메소드들 jsdoc 추가
    create, read, update, delete.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,24 @@
  * Represents a Todo App.
  * @constructor
  */
-function Todo() {}
+function Todo() {
+	/** Add a TO-DO Item.
+	 * @member {@function} 
+	 */
+	 this.create = () => {}
+
+	 /** Read All of the TO-DO Items.
+	  * @member {@function}
+	 */
+	 this.read = () => {}
+
+	/** Change the tag content of the TO-DO Item.
+	 * @member {@function}
+	*/
+	 this.update = () => {}
+ 
+	 /** Remove one or all of the TO-DO items. 
+	  * Also remove one tag or all of the tags of the TO-DO Item.
+	 */
+	 this.delete = () => {}
+}
