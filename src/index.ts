c750362f9ea81ca0b34aadb0b5d61bf0b73adfe7
hon9g <mimmh93@gmail.com>
2022-09-23 01:45:29 +0900

    [refactor] 개발용 더미데이터 삭제

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
 import TodoModel from "./domain/TodoList"
-import view from "./presentation/index"
+import TodoView from "./presentation/index"
 
-const todoList = new TodoModel()
-todoList.create({
-	description: "첫번째 할 일",
-})
-todoList.create({
-	description: "두번째 할 일",
-})
-todoList.create({
-	description: "세번째 할 일",
-	isDone: true,
-})
-
-view({ model: todoList })
+TodoView({ model: new TodoModel() })
