e1035e1b377d67676a5483d16b894b1acca5eaa2
hon9g <mimmh93@gmail.com>
2022-09-22 19:48:05 +0900

    [dependency] useState 사용을 위해 react 버전 업

--- src/presentation/index.tsx
+++ src/presentation/index.tsx
@@ -1,6 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
 import TodoApp from "./views/TodoApp"
 
-export default ({ model }) => ReactDOM.render(<TodoApp model={model} />, document.getElementById("root"))
+export default ({ model }) => {
+    const rootElement = document.getElementById("root");
+    const root = createRoot(rootElement);
+    root.render(
+      <StrictMode>
+        <TodoApp model={model} />
+      </StrictMode>
+    );
+}
+
+
