676ae0491b90aa070073296c2241a95f64b70d12
hon9g <mimmh93@gmail.com>
2022-09-23 14:33:08 +0900

    [feat] 할일 app에서 main영역과 edit영역을 분리하여 라우팅
    - react router dom 적용
    - main영역과 edit 영역 분리

--- src/presentation/views/TodoApp.tsx
+++ src/presentation/views/TodoApp.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { Routes, Route } from "react-router-dom"
 
 import Todo, { TodoItemProps } from "../../domain/TodoList"
 import Checkbox from "../components/Checkbox"
@@ -9,7 +10,7 @@ interface Props {
     model: Todo
 }
 
-const App = ({ model }: Props) => {
+const Main = ({ model }: Props) => {
     const [todoList, setTodoList] = useState(model.readAll())
     const [newTodoDescription, setNewTodoDescription] = useState('')
     const addTodoItem = () => {
@@ -25,28 +26,44 @@ const App = ({ model }: Props) => {
         setNewTodoDescription(event.target.value)
     }
     return (
+    <>
+    <List>
+        {todoList.map(todoItem => <Checkbox
+            label={todoItem.description}
+            value={todoItem.id}
+            isChecked={todoItem.isDone}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                model.update({
+                    [TodoItemProps.ID]: todoItem.id,
+                    property: TodoItemProps.IS_DONE,
+                    newValue: event.target.checked,
+                })
+                setTodoList(model.readAll())
+            }}
+            key={todoItem.id.toString()}
+            />)}
+    </List>
+    <TextInput
+        value={newTodoDescription}
+        onChange={onChangeNewTodoDescription}
+        onSubmit={addTodoItem}
+    />
+    </>
+    )
+}
+
+const Edit = ({ model }: Props) => {
+    return <h3>on Edit Mode</h3>
+}
+
+const App = ({ model }: Props) => {
+    return (
         <>
         <h1>To ~ do ~ !</h1>
-        <List>
-            {todoList.map(todoItem => <Checkbox
-                label={todoItem.description}
-                value={todoItem.id}
-                isChecked={todoItem.isDone}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    model.update({
-                        [TodoItemProps.ID]: todoItem.id,
-                        property: TodoItemProps.IS_DONE,
-                        newValue: event.target.checked,
-                    })
-                    setTodoList(model.readAll())
-                }}
-                />)}
-        </List>
-        <TextInput
-            value={newTodoDescription}
-            onChange={onChangeNewTodoDescription}
-            onSubmit={addTodoItem}
-        />
+        <Routes>
+            <Route path="/" element={<Main model={model} />} />
+            <Route path="/edit" element={<Edit model={model} />} />
+        </Routes>
         </>
     )
 }
