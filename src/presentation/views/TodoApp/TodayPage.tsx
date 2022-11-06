72edd1ae7e6ca4ba6a30fd1bb6018e9ba8b0ac4d
hon9g <mimmh93@gmail.com>
2022-11-06 23:39:54 +0900

    [feat] 오늘 페이지에 메인으로 돌아가는 링크 추가

--- src/presentation/views/TodoApp/TodayPage.tsx
+++ src/presentation/views/TodoApp/TodayPage.tsx
@@ -1,14 +1,19 @@
 import React from "react"
+import { Link } from "react-router-dom"
+
+import { PATH } from "./index"
 
 enum PAGE_CONTENTS {
   TITLE = "오늘 할 일",
+  GOTO_HOME = "모든 할 일 보러가기",
 }
 
 interface Props {}
 
 const TodayPage = ({}: Props) => {
   return <>
-  <h1>{PAGE_CONTENTS.TITLE}</h1>
+  <h1 className="title">{PAGE_CONTENTS.TITLE}</h1>
+  <Link to={PATH.HOME} className="link">{PAGE_CONTENTS.GOTO_HOME}</Link>
   </>
 }
 
